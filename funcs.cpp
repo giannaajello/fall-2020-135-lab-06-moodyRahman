0546de92bed5740325d2acbfd210ddaf2941dbeb
Moody Rahman <mrahman01@stuy.edu>
2020-10-11 13:44:43 -0400

    formatting

--- funcs.cpp
+++ funcs.cpp
@@ -76,7 +76,6 @@ std::string decryptCaesar(std::string ciphertext, int rshift)
 	return encryptCaesar(ciphertext, -1 * rshift);
 }
 
-
 std::string decryptVigenere(std::string ciphertext, std::string keyword)
 {
 	string out = "";
