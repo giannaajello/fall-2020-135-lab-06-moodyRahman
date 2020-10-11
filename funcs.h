34a2b40c6198199f0c6d53296ce8c2e85e78c8a5
Moody Rahman <mrahman01@stuy.edu>
2020-10-11 13:44:48 -0400

    headers

--- funcs.h
+++ funcs.h
@@ -3,3 +3,8 @@
 // add prototypes here
 void test_ascii();
 char shiftChar(char c, int rshift);
+std::string encryptCaesar(std::string plaintext, int rshift);
+std::string encryptVigenere(std::string plaintext, std::string keyword);
+int chartoint(char in);
+std::string decryptCaesar(std::string ciphertext, int rshift);
+std::string decryptVigenere(std::string ciphertext, std::string keyword);
