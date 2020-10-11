c4c72f8cff2b94047d0e8d82fabea25dd226e442
Moody Rahman <mrahman01@stuy.edu>
2020-10-11 13:52:54 -0400

    ceasaer tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 
-
 #include "funcs.h"
+
+using std::string;
+
+void moodtest(string func, string in, string expected)
+{
+  std::cout << func << ": got  " << in << "     " << "expected  " << expected;
+  if (in == expected)
+  {
+    std::cout << "   GOOD RESULT!";
+  }
+  else
+  {
+    /* code */
+  }
+
+  std::cout << std::endl;
+}
 
 int main()
 {
-  char out = shiftChar('a', 10);
-  std::cout << out << std::endl;
+
+  string out = encryptCaesar("Way to Go!", 5);
+
+  moodtest("encryptCeasar(\"abcde123!?\", 1)", encryptCaesar("abcde123!?", 1), "bcdef123!?");
+  moodtest("decryptCeaser(\"bcdef123!?\")", decryptCaesar("bcdef123!?", 1), "abcde123!?");
+
   return 0;
 }
